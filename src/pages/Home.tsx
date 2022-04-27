6d519a17513b5bf6549ca60e210e4272cdfb5422
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 11:19:26 +0545

    added some more content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import codeGif from "../assets/codeGirl.gif";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="px-5 py-5 flex flex-col font-footer justify-center items-center sm:flex-row sm:justify-evenly">
+      <div>
+        <h1 className="text-tertiary  text-7xl font-bold text-center  w-72">
+          Hi! I'm Swostika.
+        </h1>
+        <p className="text-secondary text-4xl font-extralight font-mono">
+          A Software Developer.
+        </p>
+      </div>
+      <img src={codeGif} />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
