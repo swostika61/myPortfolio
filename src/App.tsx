265fb7ffcc6a31b1c77537a8f89dc2d64f5109be
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 08:48:41 +0545

    added the navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route, Link } from "react-router-dom";
-import './App.css';
+import "./App.css";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Routes>
+    <>
+      <Navigation />
+      <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+    </>
   );
 }
 
