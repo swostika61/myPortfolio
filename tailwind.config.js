e1a3e3bd9363abc722f8f2ee413b09f1199fa5c6
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 08:44:34 +0545

    added the custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#2d2d2d",
+      secondary: "#8d2663",
+      tertiary: "#ababab",
+      black: "#000000",
+      white: "#ffffff",
+    },
+    fontFamily: {
+      header: ['Water Brush', 'cursive'],
+      sans: ['Cairo', "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
-}
+};
