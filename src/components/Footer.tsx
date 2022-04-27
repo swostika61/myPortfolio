b45d340c56c1c9d8a0ba5abd1c2149a4949ed127
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 11:11:06 +0545

    added the contents and some styling

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <div className="bg-primary min-h-11 flex-column justify-center items-center text-sm px-5 py-3 text-tertiary font-footer ">
+          <p>&copy; Swostika Lamichhane. All Rights Reserved.</p>
+          <p>Developed by Swostika</p>
+    </div>
+  )
+}
+
+export default Footer
