87ab51c8bccb68df59729b91dda67355c3907545
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 08:52:38 +0545

    added the about route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </>
   );
