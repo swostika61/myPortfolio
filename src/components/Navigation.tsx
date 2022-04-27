01ac3861f25d05e4181592705ef801f1c01e3256
swostika61 <swostikalamichhane61@gmail.com>
2022-04-27 09:00:06 +0545

    added the links

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <div className="bg-primary h-11 flex justify-between items-center px-3 text-center text-white">
+      <div className=" text-2xl font-header font-medium text-tertiary">
+        <Link to="/"><span className="text-secondary">Swos</span>tika</Link>
+      </div>
+      <div className="flex justify-between text-lg font-medium font-sans">
+        <Link to="/">Home</Link>
+        <Link to="/about" className="ml-5">About</Link>
+        <Link to="/contact" className="ml-5">Contact</Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navigation
+export default Navigation;
