cf5a6ede6ca0f9685b46298cd63a59e45879a0ac
swostika61 <swostikalamichhane61@gmail.com>
2022-04-28 13:58:16 +0545

    added github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-      <div className="bg-primary min-h-11 flex-column justify-center items-center text-sm px-5 py-3 text-tertiary font-footer ">
-          <p>&copy; Swostika Lamichhane. All Rights Reserved.</p>
-          <p>Developed by Swostika</p>
+    <div className="bg-primary min-h-11 flex-column justify-center items-center text-sm px-5 py-3 text-tertiary font-footer ">
+      <div>
+        <p>&copy; Swostika Lamichhane. All Rights Reserved.</p>
+        <p>Developed by Swostika</p>
+      </div>
+      <div>
+        <Link to="https://github.com/swostika61" target="_blank">
+          <i className="fa-brands fa-github"></i>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
