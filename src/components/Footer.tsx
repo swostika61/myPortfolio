9f060d9c64ca34b5e87e64d3cb092c66ddf3a6bb
swostika61 <swostikalamichhane61@gmail.com>
2022-04-28 15:48:53 +0545

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="bg-primary min-h-11 flex justify-between items-center text-sm px-5 py-3 text-tertiary font-footer ">
-      <div className="flex flex-column ">
+      <div className="flex flex-col ">
         <p>&copy; Swostika Lamichhane. All Rights Reserved.</p>
         <p>Developed by Swostika</p>
       </div>
