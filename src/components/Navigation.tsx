7c59116c76c4e7c77c5a448b22242a83a6508a2f
swostika61 <swostikalamichhane61@gmail.com>
2022-04-28 13:42:28 +0545

    some changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,16 +1,41 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navigation = () => {
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="bg-black h-14 py-6 px-5 flex justify-between items-center  text-center text-white">
-      <div className=" text-2xl font-header font-medium text-tertiary">
-        <Link to="/"><span className="text-secondary">Swos</span>tika</Link>
+      <div className=" text-3xl font-header font-medium text-tertiary">
+        <Link to="/">
+          <span className="text-secondary">Swos</span>tika
+        </Link>
       </div>
-      <div className="flex justify-between text-lg font-medium font-sans">
-        <Link to="/">Home</Link>
-        <Link to="/about" className="ml-5">About</Link>
-        <Link to="/contact" className="ml-5">Contact</Link>
+      <div className="flex justify-between text-white text-lg font-medium font-sans">
+        <Link
+          to="/"
+          className={`nav-links ${
+            location.pathname == "/" ? "nav-active" : null
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={`nav-links ml-5 ${
+            location.pathname == "/about" ? "nav-active" : null
+          }`}
+        >
+          About
+        </Link>
+        <Link
+          to="/projects"
+          className={`nav-links ml-5 ${
+            location.pathname == "/projects" ? "nav-active" : null
+          }`}
+        >
+          Projects
+        </Link>
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import codeGif from "../assets/codeGirl.gif";
+import SplitText from "../components/SplitText";
+
 const Home = () => {
   return (
-    <div className="px-5 py-5 flex flex-col font-footer justify-center items-center sm:flex-row sm:justify-evenly">
-      <div>
-        <h1 className="text-tertiary  text-7xl font-bold text-center  w-72">
+    <div className=" h-screen px-5 py-5 flex flex-col font-footer justify-center items-center sm:flex-row sm:justify-evenly">
+      <div className="my-8">
+        <h1 className="text-tertiary text-7xl font-bold text-center w-full sm:w-72 ">
           Hi! I'm Swostika.
         </h1>
-        <p className="text-secondary text-4xl font-extralight font-mono">
-          A Software Developer.
+
+        <p className="bounce text-secondary text-3xl text-center font-extralight mt-3 w-full  font-mono sm:text-4xl">
+          <SplitText copy="I Am A Software Developer." role="heading" />
         </p>
       </div>
       <img src={codeGif} />
