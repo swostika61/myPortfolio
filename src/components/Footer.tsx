b5f52438fb05d444bc5974a2c67784f3801b68af
swostika61 <swostikalamichhane61@gmail.com>
2022-04-29 10:41:13 +0545

    styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-primary min-h-11 flex flex-col justify-between items-center text-sm px-5 py-3 text-tertiary font-footer sm:flex-row">
+    <div className="bg-primary min-h-11 flex flex-col justify-between text-sm px-5 py-3 text-tertiary font-footer sm:flex-row sm:items-center">
       <div className="flex flex-col">
         <p>&copy; Swostika Lamichhane. All Rights Reserved.</p>
         <p>Developed by Swostika</p>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center mt-3 sm:mt-0">
         <a href="https://github.com/swostika61" target="_blank">
           <i className="fa-brands fa-github"></i>
         </a>
